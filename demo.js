9e59d4cf8d216a8fde5c95768e57ff4035e785b0
wangning <wangning@eqxiu.com>
2020-12-30 16:16:41 +0800

    尝试触发

--- demo.js
+++ demo.js
@@ -27,6 +27,7 @@ http.createServer((req, res) => {
 })
 handler.on('push', () => {
     console.log('已监听到push事件')
+    console.log('hahahahahha')
     run_process('sh', ['./deploy.sh'], (text) => {
         console.log(text)
     })
