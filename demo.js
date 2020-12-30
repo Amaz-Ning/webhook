1df0c662aab8e8c88e2535f740e0992f2312958f
wangning <wangning@eqxiu.com>
2020-12-30 16:27:05 +0800

    k

--- demo.js
+++ demo.js
@@ -4,7 +4,6 @@ const {spawn} = require('child_process')
 const handler = createHandler({
     path: '/',
     secret: 'helloworld'
-
 })
 const run_process = (cmd, args, callback) => {
     const child = spawn(cmd, args)
