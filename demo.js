b07816bbf82ac221c3d2d328c1cb65f5b29c01ed
wangning <wangning@eqxiu.com>
2020-12-30 16:23:51 +0800

    修改代码

--- demo.js
+++ demo.js
@@ -4,11 +4,11 @@ const {spawn} = require('child_process')
 const handler = createHandler({
     path: '/',
     secret: 'helloworld'
-    
+
 })
 const run_process = (cmd, args, callback) => {
     const child = spawn(cmd, args)
-    const resp = '';
+    let resp = '';
     child.stdout.on('data', (buffer) => {
         resp += buffer.toString();
     })
