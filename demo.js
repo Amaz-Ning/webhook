f9838327a777588cc343c5d74ba51db644040862
wangning <wangning@eqxiu.com>
2020-12-30 16:10:31 +0800

    添加shell脚本

--- demo.js
+++ demo.js
@@ -1,12 +1,23 @@
 const http = require('http');
 const createHandler = require('github-webhook-handler')
+const {spawn} = require('child_process')
 const handler = createHandler({
     path: '/',
     secret: 'helloworld'
 })
+const run_process = (cmd, args, callback) => {
+    const child = spawn(cmd, args)
+    const resp = '';
+    child.stdout.on('data', (buffer) => {
+        resp += buffer.toString();
+    })
+    child.stdout.on('end', () => {
+        callback(resp)
+    })
+}
 http.createServer((req, res) => {
     handler(req, res, (err) => {
-        
+
         res.statusCode = 404
         res.end('no such location')
     })
@@ -15,4 +26,7 @@ http.createServer((req, res) => {
 })
 handler.on('push', () => {
     console.log('已监听到push事件')
+    run_process('sh', ['./deploy.sh'], (text) => {
+        console.log(text)
+    })
 })
